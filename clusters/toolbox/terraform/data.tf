dc7c4b3af1b6b70bef89e368a8e09c88acee0b12
Mario Garcia <mario.ggar@gmail.com>
2023-05-09 20:01:44 +0200

    feat: adding terraform cloud

--- clusters/toolbox/terraform/data.tf
+++ clusters/toolbox/terraform/data.tf
@@ -1,3 +1,5 @@
 data "aws_route53_zone" "cluster_hosted_zone" {
   name = local.cluster_domain_name
 }
+
+data "aws_caller_identity" "current" {}
--- clusters/toolbox/terraform/provider.tf
+++ clusters/toolbox/terraform/provider.tf
@@ -1,9 +1,16 @@
 provider "aws" {
-  region = "eu-central-1"
+  region  = "eu-central-1"
   profile = "toolbox.secuoyas.com"
 }
 
 terraform {
+   cloud {
+    organization = "secuoyas"
+
+    workspaces {
+      name = "toolbox"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
