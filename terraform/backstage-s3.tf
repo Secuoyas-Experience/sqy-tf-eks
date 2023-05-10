a129d49a3d31bc47919eaa18deca9dcff37a47c8
Mario Garcia <mario.ggar@gmail.com>
2023-05-10 10:51:19 +0200

    fix: don't create access key by default

--- terraform/backstage-s3.tf
+++ terraform/backstage-s3.tf
@@ -122,6 +122,7 @@ module "backstage_reader_user_group" {
   name                     = "${local.prefix}-readers"
   group_users              = [module.backstage_reader_iam_user.iam_user_name]
   custom_group_policy_arns = [aws_iam_policy.backstage_read_policy.arn]
+  create_iam_access_key    = false
 }
 
 module "backstage_writer_iam_user" {
@@ -130,6 +131,7 @@ module "backstage_writer_iam_user" {
   force_destroy                 = true
   password_reset_required       = true
   create_iam_user_login_profile = false
+  create_iam_access_key         = false
 }
 
 module "backstage_writer_user_group" {
