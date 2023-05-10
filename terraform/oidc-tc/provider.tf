428f1369498bc6cc81abcdc089b1a0d1402945ae
Mario Garcia <mario.ggar@gmail.com>
2023-05-10 21:24:10 +0200

    fix: don't know how to sync already created oidc provider

--- terraform/oidc-tc/provider.tf
+++ terraform/oidc-tc/provider.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_openid_connect_provider" "terraform_cloud_oidc_provider" {
-  url             = "https://app.terraform.io"
-  client_id_list  = ["aws.workload.identity"]
-  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
-}
+# resource "aws_iam_openid_connect_provider" "terraform_cloud_oidc_provider" {
+#   url             = "https://app.terraform.io"
+#   client_id_list  = ["aws.workload.identity"]
+#   thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
+# }
