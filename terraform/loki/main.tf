17b22a664d25cc2f1dd2b146181c73d52c3a8468
Mario Garcia <mario.ggar@gmail.com>
2023-05-14 00:48:11 +0200

    fix: use proper eks auth mechanism

--- terraform/loki/main.tf
+++ terraform/loki/main.tf
@@ -13,6 +13,10 @@ data "aws_eks_cluster" "cluster" {
   name = var.cluster_name
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+}
+
 data "aws_iam_openid_connect_provider" "eks_cluster_oidc" {
   url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
