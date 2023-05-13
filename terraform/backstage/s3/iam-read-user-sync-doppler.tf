d71b32b1df68c085141d7ede2d185a3e0a03211c
Mario Garcia <mario.ggar@gmail.com>
2023-05-13 16:59:02 +0200

    fix: wrong doppler project and wrong s3 path

--- terraform/backstage/s3/iam-read-user-sync-doppler.tf
+++ terraform/backstage/s3/iam-read-user-sync-doppler.tf
@@ -1,26 +1,26 @@
 resource "doppler_secret" "backstage_read_aws_secret_key_id" {
-  project = "toolbox-k8s"
+  project = "toolbox-backstage"
   config  = "gh"
   name    = "AWS_ACCESS_KEY_ID"
   value   = module.backstage_reader_iam_user.iam_access_key_id
 }
 
 resource "doppler_secret" "backstage_read_aws_secret_access_key" {
-  project = "toolbox-k8s"
+  project = "toolbox-backstage"
   config  = "gh"
   name    = "AWS_SECRET_ACCESS_KEY"
   value   = module.backstage_reader_iam_user.iam_access_key_id
 }
 
 resource "doppler_secret" "backstage_read_aws_s3_bucket" {
-  project = "toolbox-k8s"
+  project = "toolbox-backstage"
   config  = "gh"
   name    = "AWS_S3_BUCKET"
   value   = aws_s3_bucket.backstage_bucket.id
 }
 
 resource "doppler_secret" "backstage_read_aws_default_region" {
-  project = "toolbox-k8s"
+  project = "toolbox-backstage"
   config  = "gh"
   name    = "AWS_DEFAULT_REGION"
   value   = aws_s3_bucket.backstage_bucket.region
