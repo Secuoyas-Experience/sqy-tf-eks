63379bd2ed47d8b1eca5571a4997d315dad0e57f
Mario Garcia <mario.ggar@gmail.com>
2023-05-14 04:03:43 +0200

    wip: adding token again

--- terraform/loki/main.tf
+++ terraform/loki/main.tf
@@ -64,6 +64,12 @@ provider "helm" {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster.token
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", data.data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
   }
 }
 
