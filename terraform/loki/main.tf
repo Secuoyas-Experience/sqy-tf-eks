cd9395da5b62336a459511493e0755fcb0d7d688
Mario Garcia <mario.ggar@gmail.com>
2023-05-14 19:05:41 +0200

    fix: locally worked so it seems something related to IAM

--- terraform/loki/main.tf
+++ terraform/loki/main.tf
@@ -62,17 +62,18 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
 provider "helm" {
   alias = "toolbox-cluster"
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster.token
-    # exec {
-    #   api_version = "client.authentication.k8s.io/v1beta1"
-    #   args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.id]
-    #   command     = "aws"
-    # }
   }
 }
 
