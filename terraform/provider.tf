400383a7669c98761a86302dc0fcadce11fa7b3c
Mario Garcia <mario.ggar@gmail.com>
2023-05-15 00:57:06 +0200

    fix: destroy and then test with downgrade

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -56,7 +56,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.74.0"
+      version = ">= 4.12.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
