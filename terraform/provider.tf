cb763e0a09c3673770194469487f33394b0373eb
Mario Garcia <mario.ggar@gmail.com>
2023-05-15 00:54:12 +0200

    fix: downgrading aws provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -56,7 +56,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.12.0"
+      version = "3.74.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
