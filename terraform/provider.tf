abce9af769c95588af7d03ea81797d88314a2bdb
Mario Garcia <mario.ggar@gmail.com>
2023-05-15 01:39:56 +0200

    fix: changing authentication version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -16,7 +16,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   # token                  = data.aws_eks_cluster_auth.cluster.token
   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
+    api_version = "client.authentication.k8s.io/v1"
     command     = "aws"
     args = [
       "eks",
