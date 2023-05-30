f8212e49e9213894965b0eca7e5c8dd5190e0a88
Mario G <mario.ggar@gmail.com>
2023-05-30 23:24:13 +0200

    wip: creating operator again with ns

--- 8-doppler.tf
+++ 8-doppler.tf
@@ -1,9 +1,10 @@
 resource "helm_release" "doppler_operator" {
-  namespace  = "doppler-operator-system"
-  name       = "doppler"
-  repository = "https://helm.doppler.com"
-  chart      = "doppler-kubernetes-operator"
-  version    = "1.2.5"
+  namespace        = "doppler-operator-system"
+  create_namespace = true
+  name             = "doppler"
+  repository       = "https://helm.doppler.com"
+  chart            = "doppler-kubernetes-operator"
+  version          = "1.2.5"
 
   depends_on = [
     kubernetes_namespace.argocd_namespace
