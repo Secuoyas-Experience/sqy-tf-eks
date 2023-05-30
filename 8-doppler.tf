310fbb4edc78d8301e35eca1e8870ffcdbef9f7e
Mario G <mario.ggar@gmail.com>
2023-05-30 23:03:57 +0200

    feat: adding doppler operator to cluster

---
+++ 8-doppler.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "doppler_operator" {
+  namespace        = "doppler-operator-system"
+  create_namespace = true
+  name             = "doppler"
+  repository       = "https://helm.doppler.com"
+  chart            = "doppler-kubernetes-operator"
+  version          = "1.2.5"
+
+  depends_on = [
+    kubernetes_namespace.argocd_namespace
+  ]
+}
