f3ac4d11b6dab49729352483306555f5686f85b8
Mario G <mario.ggar@gmail.com>
2023-05-31 00:00:04 +0200

    wip: not forcing namespace whatsoever

--- 8-doppler.tf
+++ 8-doppler.tf
@@ -1,24 +1,10 @@
-# created namespace first because namespace creation
-# in helm chart didn't work consistently and threw
-# errors
-# resource "kubernetes_namespace" "doppler_namespace" {
-#   metadata {
-#     name = "doppler-operator-system"
-#   }
+resource "helm_release" "doppler_operator" {
+  name             = "doppler"
+  repository       = "https://helm.doppler.com"
+  chart            = "doppler-kubernetes-operator"
+  version          = "1.2.5"
 
-#   depends_on = [
-#     kubernetes_namespace.argocd_namespace
-#   ]
-# }
-
-# resource "helm_release" "doppler_operator" {
-#   namespace        = "doppler-operator-system"
-#   name             = "doppler"
-#   repository       = "https://helm.doppler.com"
-#   chart            = "doppler-kubernetes-operator"
-#   version          = "1.2.5"
-
-#   depends_on = [
-#     kubernetes_namespace.doppler_namespace
-#   ]
-# }
+  depends_on = [
+    kubernetes_namespace.doppler_namespace
+  ]
+}
