9250181db58bd1560be4426dd6c65f14951aa81d
Mario G <mario.ggar@gmail.com>
2023-05-31 00:03:07 +0200

    fix: wrong dependency resource

--- 8-doppler.tf
+++ 8-doppler.tf
@@ -1,10 +1,10 @@
 resource "helm_release" "doppler_operator" {
-  name             = "doppler"
-  repository       = "https://helm.doppler.com"
-  chart            = "doppler-kubernetes-operator"
-  version          = "1.2.5"
+  name       = "doppler"
+  repository = "https://helm.doppler.com"
+  chart      = "doppler-kubernetes-operator"
+  version    = "1.2.5"
 
   depends_on = [
-    kubernetes_namespace.doppler_namespace
+    kubernetes_namespace.argocd_namespace
   ]
 }
