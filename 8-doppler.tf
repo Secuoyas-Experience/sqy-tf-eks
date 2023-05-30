dd0597e0eb94bc886d90e126763a5a578cf899cc
Mario G <mario.ggar@gmail.com>
2023-05-30 23:32:06 +0200

    wip: creating doppler namespace before applying helm chart

--- 8-doppler.tf
+++ 8-doppler.tf
@@ -1,12 +1,25 @@
-# resource "helm_release" "doppler_operator" {
-#   namespace        = "doppler-operator-system"
-#   create_namespace = true
-#   name             = "doppler"
-#   repository       = "https://helm.doppler.com"
-#   chart            = "doppler-kubernetes-operator"
-#   version          = "1.2.5"
+# created namespace first because namespace creation
+# in helm chart didn't work consistently and threw
+# errors
+resource "kubernetes_namespace" "doppler_namespace" {
+  metadata {
+    name = "doppler-operator-system"
+  }
 
-#   depends_on = [
-#     kubernetes_namespace.argocd_namespace
-#   ]
-# }
+  depends_on = [
+    kubernetes_namespace.argocd_namespace
+  ]
+}
+
+resource "helm_release" "doppler_operator" {
+  namespace        = "doppler-operator-system"
+  create_namespace = false
+  name             = "doppler"
+  repository       = "https://helm.doppler.com"
+  chart            = "doppler-kubernetes-operator"
+  version          = "1.2.5"
+
+  depends_on = [
+    kubernetes_namespace.doppler_namespace
+  ]
+}
