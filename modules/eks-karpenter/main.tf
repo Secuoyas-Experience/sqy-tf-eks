df3de5e2b884ca1bc8c57f1cf06184dd0f4a43c2
Orencio Ramirez <orencio.ramirez@secuoyas.com>
2025-03-04 12:24:40 +0100

    chore: fix typo

--- modules/eks-karpenter/main.tf
+++ modules/eks-karpenter/main.tf
@@ -1,6 +1,6 @@
 module "karpenter" {
   source                 = "terraform-aws-modules/eks/aws//modules/karpenter"
-  version                = "20.33.º"
+  version                = "20.33.1"
   cluster_name           = var.cluster_name
   irsa_oidc_provider_arn = var.cluster_oidc_provider_arn
   enable_irsa            = true
