07f3e660cba1e4732066850f996ba0161079ce23
Tibin George <tibin.george@smartcardia.com>
2025-11-20 16:42:53 +0530

    chore(argocd): added argocd az variable to argocd module
    Signed-off-by: Tibin George <tibin.george@smartcardia.com>

--- modules/eks-addons/main.tf
+++ modules/eks-addons/main.tf
@@ -177,7 +177,15 @@ module "eks_addons_extra" {
         { name  = "server.ingress.tls[0].secretName",
           value = "argocd-secret-tls",
         },
-      ] : []
+      ] : [],
+    length(var.argocd_az) > 0 ? [
+      { name = "controller.nodeSelector.topology\\.kubernetes\\.io/zone", value = var.argocd_az },
+      { name = "dex.nodeSelector.topology\\.kubernetes\\.io/zone", value = var.argocd_az },
+      { name = "redis.nodeSelector.topology\\.kubernetes\\.io/zone", value = var.argocd_az },
+      { name = "repoServer.nodeSelector.topology\\.kubernetes\\.io/zone", value = var.argocd_az },
+      { name = "server.nodeSelector.topology\\.kubernetes\\.io/zone", value = var.argocd_az },
+      { name = "applicationSet.nodeSelector.topology\\.kubernetes\\.io/zone", value = var.argocd_az },
+    ] : []
     ])
   }
 
